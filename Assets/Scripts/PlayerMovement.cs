154bd4456fc1058ac8e23c76f33e2d0c5a07c7cc
Noah <dragondeath04@hotmail.com>
2024-01-30 11:04:24 +0100

    d

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float jumpPower = 1f;
+
     Rigidbody2D Rigidbody2;
+
     
     void Start()
     {
@@ -19,6 +23,11 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnMove()
     {
         
     }
