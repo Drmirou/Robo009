1c7096e8b48a0b1966e9e4708c8c1dce1ee3fa06
Oliver Åhsberg <oliver.ahsberg@learnet.se>
2024-02-02 08:45:34 +0100

    bruh

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,30 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    [SerializeField] float moveSpeed = 1f;
-    [SerializeField] float jumpPower = 1f;
-
-    Rigidbody2D Rigidbody2;
-
-    
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    
+    // Update is called once per frame
     void Update()
-    {
-        
-    }
-
-    private void FixedUpdate()
-    {
-        
-    }
-
-    private void OnMove()
     {
         
     }
