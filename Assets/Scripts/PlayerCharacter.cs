296d615d2c77915800d1decc20062b48909644af
Noah <dragondeath04@hotmail.com>
2024-02-02 08:58:32 +0100

    d

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
+    }
+
+    void OnFire()
+    {
+
     }
 }
