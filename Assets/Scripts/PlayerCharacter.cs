87d30901d45500f136834a26479ff5ea54589c61
Noah <dragondeath04@hotmail.com>
2024-02-02 10:35:51 +0100

    defwgwg

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
@@ -7,7 +8,7 @@ public class PlayerCharacter : MonoBehaviour
     private float horizontal;
     private float speed = 5f;
     private float jumpingPower = 10f;
-    private bool isFacingRight = true;
+    public bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundcheck;
@@ -26,7 +27,10 @@ public class PlayerCharacter : MonoBehaviour
 
     void Update()
     {
-        CannonCooldown -= Time.deltaTime;
+        if (CannonCooldown > 0)
+        {
+            CannonCooldown -= Time.deltaTime;
+        }
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
@@ -51,10 +55,7 @@ public class PlayerCharacter : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundcheck.position, 0.2f, groundLayer); 
-        if (CannonCooldown > 0)
-        {
-            CannonCooldown -= Time.deltaTime;
-        }
+        
     }
 
     void OnFire()
@@ -64,6 +65,11 @@ public class PlayerCharacter : MonoBehaviour
             CannonCooldown = CannonCooldownOnStart;
 
             GameObject Bellet = Instantiate(SimpleBullet, transform.position + new Vector3(0.5f, 0.0f, 0.0f), transform.rotation);
+            Rigidbody2D BelletRB = Bellet.GetComponent<Rigidbody2D>();
+            if(isFacingRight == true )
+            {
+
+            }
             Destroy(Bellet, 4f);
         }
         
