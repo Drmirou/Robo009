bf42c0042d9ff846d808c0b756765fd1ccd197d2
David Ung <david.ung@learnet.se>
2024-02-06 08:49:08 +0100

    Rah!!

--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -7,9 +7,14 @@ public class PowerUps : MonoBehaviour
     public int AddHealth = 1;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+       var PlayerScript = collision.gameObject.GetComponent<PlayerCharacter>();
+       if (PlayerScript != null)
+
         {
+          //  PlayerScript.HP += AddHealth;
+            AddHealth = 0;
             GameObject.Destroy(gameObject);
         }
     }
 }
+// Kan göra vad ni vill med powerup
