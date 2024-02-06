6209a67061d2ec4af3511a044281335e5d4f54ee
Noah <dragondeath04@hotmail.com>
2024-02-06 08:57:55 +0100

    d

---
+++ Assets/Scripts/PointAtMouseTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtMouseTest : MonoBehaviour
+{
+    public Camera cam;
+    public Rigidbody2D myRigidbody;
+    
+    
+    Vector2 mousePos;
+
+    private void Update()
+    {
+       mousePos = 
+    }
+
+}
