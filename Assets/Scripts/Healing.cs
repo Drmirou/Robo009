a6813c95928bd5b4c1a13fdf4e736bec1136271e
David Ung <david.ung@learnet.se>
2024-02-06 09:22:52 +0100

    New enemy type shit

--- Assets/Scripts/Healing.cs
+++ Assets/Scripts/Healing.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUps : MonoBehaviour
+public class Healing : MonoBehaviour
 {
     public int AddHealth = 1;
     private void OnCollisionEnter2D(Collision2D collision)
