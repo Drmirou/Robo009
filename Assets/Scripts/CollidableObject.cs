aa19808b647474e171bd0a567f0020281a6ad8b9
Oliver Åhsberg <oliver.ahsberg@learnet.se>
2024-02-09 10:05:31 +0100

    Interaction

--- Assets/Scripts/CollidableObject.cs
+++ Assets/Scripts/CollidableObject.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class CollidableObject : MonoBehaviour
-{        
+{
     private Collider2D z_Collider;
     private ContactFilter2D z_filter;
-    private List<Collider2D> z_CollidedObjects; 
+    private List<Collider2D> z_CollidedObjects = new List<Collider2D>(1);
 
-    private void Start()
+    protected virtual void Start()
     {
-        z_Collider = GetComponent<Collider2D>(); 
+        z_Collider = GetComponent<Collider2D>();
     }
 
-    private void Update()
+    protected virtual void Update()
     {
         z_Collider.OverlapCollider(z_filter, z_CollidedObjects);
-        foreach(var o in  z_CollidedObjects) 
+        foreach (var o in z_CollidedObjects)
         {
-            Debug.Log("Collided with" + o.name);
+            OnCollided(o.gameObject);   
         }
     }
+
+
+    protected virtual void OnCollided(GameObject collidedObject)
+   {
+        Debug.Log("Collided with" + collidedObject.name);
+    } 
 }
