f7efea3d9b3fd7cb032b6a4b11c19705ada2a1b8
David Ung <david.ung@learnet.se>
2024-03-01 10:45:40 +0100

    May this revamp be good ;pray;

--- Assets/Scripts/Flying enemy.cs
+++ Assets/Scripts/Flying enemy.cs
@@ -11,57 +11,6 @@ public class Enemy2 : MonoBehaviour
     public int Playerhealth;
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Move the enemy horizontally
-        transform.Translate(Vector2.left * flyingSpeed * Time.deltaTime);
-    }
-
-    // Method to handle when the enemy takes damage
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-        if (health <= 0)
-        {
-            Die();
-        }
-    }
-
-    // Method to handle the death of the enemy
-    void Die()
-    {
-        
-        Destroy(gameObject);
-    }
-
-    // Method to handle collision with other objects
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        // If the collided object is tagged as "Player"
-        if (other.CompareTag("Player"))
-        {
-            Attack(other.gameObject);
-        }
-    }
-    
-
-    
-    void Attack(GameObject player)
-    {
-        
-        PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
-      
-       if (playerHealth != null)
-        {
-          
-        //  playerHealth.TakeDamage(attackPower);
-        }
-    }
-
-    private class PlayerHealth
-    {
-    }
 }
 
 // FREEDOM RAHHHH :BIRD: :BRID:
