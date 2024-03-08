21a26d747e8cbc88b2db9be981ff7d74b206a97e
David Ung <david.ung@learnet.se>
2024-03-08 10:24:03 +0100

    Npc

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -1,46 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine; 
+using Cinemachine;
 
 public class CameraShake : MonoBehaviour
 {
-   private CinemachineVirtualCamera CinemachineVirtualCamera;
-   private float ShakeIntensity = 1.0f;
+    private CinemachineVirtualCamera CinemachineVirtualCamera;
+    private float ShakeIntensity = 1.0f;
     private float ShakeTime = 0.2f;
 
     private float timer;
     private CinemachineBasicMultiChannelPerlin _cbmcp;
-    
-    
-    
-    
+
+
+
+
     void Awake()
     {
         CinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
 
-    public void ShakeCamera(){   
-          CinemachineBasicMultiChannelPerlin _cbmcp = CinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-          _cbmcp.m_AmplitudeGain = ShakeIntensity;
+    public void ShakeCamera() {
+        CinemachineBasicMultiChannelPerlin _cbmcp = CinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _cbmcp.m_AmplitudeGain = ShakeIntensity;
 
 
-        timer = ShakeTime; 
-    
+        timer = ShakeTime;
+
     }
-  
-    void StopShake(){
+
+    void StopShake() {
         CinemachineBasicMultiChannelPerlin _cbmcp = CinemachineVirtualCamera
 
-    
-    
 
-    
+
+
+
 
 
     // Update is called once per frame
     void Update()
-    {
-        
+        {
+
+        }
     }
 }
---
+++ Assets/Scripts/Friendly mob.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Friendly_Mob : MonoBehaviour
+{
+    public string[] dialogueLines;
+    private int currentLine = 0;
+
+    public float WanderDistance = 2.0f;
+    public float movespeed = 1.0f;
+
+}
+
+ 
