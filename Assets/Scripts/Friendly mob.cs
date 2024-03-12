c49fc18ecf879ae77ddde8c7923b60bf060cc003
David Ung <david.ung@learnet.se>
2024-03-12 09:01:01 +0100

    Omg it talk

--- Assets/Scripts/Friendly mob.cs
+++ Assets/Scripts/Friendly mob.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Friendly_Mob : MonoBehaviour
 {
     public string[] dialogueLines;
-    private int currentLine = 0;
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public int index;
 
-    public float WanderDistance = 2.0f;
-    public float movespeed = 1.0f;
+    public float wordspeed;
+    public bool playerIsClose;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        { 
+     //    if(dialoguePanel.
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+
+        }
+    }
 }
 
  
