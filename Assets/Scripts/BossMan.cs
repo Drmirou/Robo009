ea52f7ed31714d292947dac4c1b52697c29df1db
David Ung <david.ung@learnet.se>
2024-03-22 08:44:54 +0100

    W Boss man

--- Assets/Scripts/BossMan.cs
+++ Assets/Scripts/BossMan.cs
@@ -5,5 +5,27 @@ using UnityEngine;
 public class Bosscode : MonoBehaviour
 {
     public PlayerHealth playerHealth;
+    private GameObject player;
+    public bool chase = false;
+    public float HP;
+    public int damage = 2;
+    public float dash = 1;
+
+
+    public void TakeDamage(int aHPValue)
+    {
+        HP += aHPValue;
+
+        if (HP < 0)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
 
 }
