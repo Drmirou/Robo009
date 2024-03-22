5aba5a5f0eb8bd2618bb7bbab24a90c92fbc1115
David Ung <david.ung@learnet.se>
2024-03-22 09:00:52 +0100

    L Bossman

--- Assets/Scripts/BossMan.cs
+++ Assets/Scripts/BossMan.cs
@@ -10,6 +10,7 @@ public class Bosscode : MonoBehaviour
     public float HP;
     public int damage = 2;
     public float dash = 1;
+    public float speed;
 
 
     public void TakeDamage(int aHPValue)
@@ -26,6 +27,14 @@ public class Bosscode : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player");
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damage);
+        }
     }
 
 }
