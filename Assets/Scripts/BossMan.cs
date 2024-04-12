2d1a7f254e683d5c1ddc0b53243f83bf18b13cfc
David Ung <david.ung@learnet.se>
2024-04-12 09:50:54 +0200

    Boss rework

--- Assets/Scripts/BossMan.cs
+++ Assets/Scripts/BossMan.cs
@@ -2,49 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bosscode : MonoBehaviour
+public class Boss_run: MonoBehaviour
 {
-    public PlayerHealth playerHealth;
-    private GameObject player;
-    public bool chase = false;
-    public float HP;
-    public int damage = 2;
-    public float speed;
-    public float dashCooldown = 10f; 
-    public int maxDashes = 3;
-    public float dashSpeed = 10f; 
-    public float dashDuration = 0.5f; 
-    private bool isDashing = false; 
-    private int remainingDashes;
-    private float dashTimer = 2f; 
-
-
-    public void TakeDamage(int aHPValue)
-    {
-        HP += aHPValue;
-
-        if (HP < 0)
-        {
-            GameObject.Destroy(gameObject);
-        }
-    }
-
-    private void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            playerHealth.TakeDamage(damage);
-        }
-    }
-
-
-
 
 
     // You guys are such a winners on god!!!
