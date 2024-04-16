ca1ef07b6e423dcaa2526fbabbd50f85c784c0c7
David Ung <david.ung@learnet.se>
2024-04-16 08:55:05 +0200

    Fixed

--- Assets/Scripts/Boss run.cs
+++ Assets/Scripts/Boss run.cs
@@ -5,25 +5,37 @@ using UnityEngine.Animations;
 
 public class Boss_run : StateMachineBehaviour
 {
+
+
+	public float speed = 2.5f;
+    public float attackRange = 3f;
+
     Transform player;
     Rigidbody2D rb;
     Bosscode boss;
 
+
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        player = GameObject.FindGameObjectsWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Bosscode>();
+
     }
+
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Vector2 target = new Vector2(player.position.x, rb.position.y);
-        Vector2.MoveTowards(rb.position.target.speed * Time.fixedDeltaTime);
-    }
+        boss.LookAtPlayer();
 
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-       
-    }
-    // You guys are such a winners on god!!!
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+
+        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
+        // You guys are such a winners on god!!!
+    } 
 }
