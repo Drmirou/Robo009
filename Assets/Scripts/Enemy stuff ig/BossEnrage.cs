898fcadad1155890cff357c2d342df1992a44fef
David Ung <david.ung@learnet.se>
2024-04-23 10:10:52 +0200

    hmm

---
+++ Assets/Scripts/Enemy stuff ig/BossEnrage.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnrage : MonoBehaviour
+{
+ //   public Bosshealth Bosshealth;
+	
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+ //   {
+ //       animator.GetComponent<Bosshealth>().isInvulnerable = true;
+ //   }
+
+  
+ //   override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+ //   {
+
+ //   }
+
+    
+ //   override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+ //   {
+ //       animator.GetComponent<Bosshealth>().isInvulnerable = false;
+ //   }
+}
+
