7d1931fa8cde725e0f16bb405c1e8bb238d685e0
David Ung <david.ung@learnet.se>
2024-04-30 09:12:30 +0200

    hmm, yes, attack

--- Assets/Scripts/Enemy stuff ig/Boss melee or something.cs
+++ Assets/Scripts/Enemy stuff ig/Boss melee or something.cs
@@ -11,4 +11,25 @@ public class Bossmeleeorsomething : MonoBehaviour
     public float attackRange = 1f;
     public LayerMask attackMask;
 
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {
+            colInfo.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+        }
+    
+    }
+
+    private void EnragedAttack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+    }
 }
+
